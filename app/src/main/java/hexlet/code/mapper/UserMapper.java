ae4100aea1092afa95c9e7fe06ac450529b7f935
Iliatar <a.u.tarasov@gmail.com>
2024-10-07 21:28:10 +0300

    add password hashing

--- app/src/main/java/hexlet/code/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/mapper/UserMapper.java
@@ -4,11 +4,15 @@ import hexlet.code.dto.UserCreateDTO;
 import hexlet.code.dto.UserDTO;
 import hexlet.code.dto.UserUpdateDTO;
 import org.mapstruct.Mapper;
-import hexlet.code.model.User;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.MappingTarget;
+import hexlet.code.model.User;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Mapper (
         uses = { JsonNullableMapper.class },
@@ -20,4 +24,17 @@ public abstract class UserMapper {
     public abstract UserDTO map(User user);
     public abstract User map(UserCreateDTO createDTO);
     public abstract void update(UserUpdateDTO updateDTO, @MappingTarget User user);
+
+    @BeforeMapping
+    public void encryptPassword(UserCreateDTO createDTO) {
+        var password = createDTO.getPassword();
+        try {
+            var md = MessageDigest.getInstance("SHA-512");
+            password = md.digest(password.getBytes()).toString();
+            System.out.println("digest password = " + password);
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+        createDTO.setPassword(password);
+    }
 }
