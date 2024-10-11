1babf0d00ce1e041fdfbbf1cacecca35359be851
Iliatar <a.u.tarasov@gmail.com>
2024-10-11 19:00:48 +0300

    auth tests completed

--- app/src/test/java/hexlet/code/controller/AuthenticationControllerTests.java
+++ app/src/test/java/hexlet/code/controller/AuthenticationControllerTests.java
@@ -1,21 +1,20 @@
 package hexlet.code.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-//import hexlet.code.dto.AuthRequest;
+import hexlet.code.dto.AuthRequest;
 import hexlet.code.service.CustomUserDetailsService;
-//import hexlet.code.utils.FakerTestData;
-//import org.junit.jupiter.api.Test;
+import hexlet.code.utils.FakerTestData;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,9 +28,16 @@ public class AuthenticationControllerTests {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
-    private SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor token;
+    @Test
+    public void testLoginFailed() throws Exception {
+        var request = get("/api/users")
+                .header("Authorization", "Bearer tram pam pam");
 
-    /*@Test
+        mockMvc.perform(request)
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
     public void testLogin() throws Exception {
         var testUser = FakerTestData.getFakerUser();
         userDetailsService.createUser(testUser);
@@ -48,8 +54,17 @@ public class AuthenticationControllerTests {
                 .andExpect(status().isOk())
                 .andReturn();
 
-        var actualToken = result.getResponse().getContentAsString();
-        var expectedToken = jwt().jwt(builder -> builder.subject(testUser.getEmail()));
-        assertThat(actualToken).isEqualTo(expectedToken);
-    }*/
+        var token = result.getResponse().getContentAsString();
+
+        request = get("/api/users")
+                .header("Authorization", "Bearer " + token);
+
+        result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isArray();
+//        var expectedToken = jwt().jwt(builder -> builder.subject(testUser.getEmail()));
+//        assertThat(actualToken).isEqualTo(expectedToken);
+    }
 }
