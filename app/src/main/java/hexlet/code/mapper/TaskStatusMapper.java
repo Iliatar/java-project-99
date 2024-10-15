478d33eddccff9c57e8cda2d33b74da92362aa0b
Iliatar <a.u.tarasov@gmail.com>
2024-10-15 07:54:49 +0300

    add TaskStatusMapper

---
+++ app/src/main/java/hexlet/code/mapper/TaskStatusMapper.java
@@ -0,0 +1,21 @@
+package hexlet.code.mapper;
+
+import hexlet.code.dto.TaskStatusCreateDTO;
+import hexlet.code.dto.TaskStatusDTO;
+import hexlet.code.dto.TaskStatusUpdateDTO;
+import hexlet.code.model.TaskStatus;
+import org.mapstruct.*;
+
+@Mapper(
+        uses = {JsonNullableMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskStatusMapper {
+    public abstract TaskStatusDTO map(TaskStatus status);
+
+    public abstract TaskStatus map(TaskStatusCreateDTO createDTO);
+
+    public abstract void update(TaskStatusUpdateDTO updateDTO, @MappingTarget TaskStatus model);
+}
