7b0cabb031da1f8cd602003e134764bc8fec07b9
Iliatar <a.u.tarasov@gmail.com>
2024-10-17 07:33:32 +0300

    add Task model

---
+++ app/src/main/java/hexlet/code/model/Task.java
@@ -0,0 +1,41 @@
+package hexlet.code.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private long id;
+
+    @NotBlank
+    private String name;
+
+    private long index;
+
+    private String description;
+
+    @NotNull
+    private TaskStatus taskStatus;
+
+    private User assignee;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+}
