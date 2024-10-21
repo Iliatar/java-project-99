f731c3425e1ee32b55a0a523b59ef7d6d6723367
Iliatar <a.u.tarasov@gmail.com>
2024-10-21 07:47:08 +0300

    add LabelDTOs

---
+++ app/src/main/java/hexlet/code/dto/LabelCreateDTO.java
@@ -0,0 +1,12 @@
+package hexlet.code.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+public class LabelCreateDTO {
+    @Length(min = 3, max = 1000)
+    private String name;
+}
