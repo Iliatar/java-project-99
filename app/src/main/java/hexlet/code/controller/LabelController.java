9d1d1b198408f5fe0152daf560aae290eb704667
Iliatar <a.u.tarasov@gmail.com>
2024-10-21 08:05:32 +0300

    add Label Repository

---
+++ app/src/main/java/hexlet/code/controller/LabelController.java
@@ -0,0 +1,71 @@
+package hexlet.code.controller;
+
+import hexlet.code.dto.*;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.LabelMapper;
+import hexlet.code.repository.LabelRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/labels")
+public class LabelController {
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
+    private LabelMapper labelMapper;
+
+    @GetMapping(path = "")
+    public ResponseEntity<List<LabelDTO>> index() {
+        var statusList = labelRepository.findAll()
+                .stream()
+                .map(labelMapper::map)
+                .toList();
+
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(statusList.size()))
+                .body(statusList);
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public LabelDTO show(@PathVariable Long id) {
+        var taskStatus = labelRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Label with id " + id + " not found"));
+
+        return labelMapper.map(taskStatus);
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public LabelDTO create(@RequestBody @Valid LabelCreateDTO createDTO) {
+        var model = labelMapper.map(createDTO);
+        labelRepository.save(model);
+        var dto = labelMapper.map(model);
+        return dto;
+    }
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public LabelDTO update(@RequestBody @Valid LabelUpdateDTO updateDTO, @PathVariable Long id) {
+        var model = labelRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+        labelMapper.update(updateDTO, model);
+        labelRepository.save(model);
+        var dto = labelMapper.map(model);
+        return dto;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        labelRepository.deleteById(id);
+    }
+
+}
